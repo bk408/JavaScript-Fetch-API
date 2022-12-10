f1a49a42d7a69e8e1056a65da7e9e4d0fff54dac
bk408 <bhavyakhatri1912@gmail.com>
2022-12-10 22:06:53 +0530

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://fakestoreapi.com/products').then((data) => {
+ fetch('https://fakestoreapi.com/products ').then((data)=>{
     // console.log(data);
     return data.json();
 }).then((completedata) => {
@@ -7,8 +7,7 @@ fetch('https://fakestoreapi.com/products').then((data) => {
 
     let data1 = "";
     completedata.map((values) => {
-        data1 += `<div id="cards">
-        <div class="card">
+        data1 += `<div class="card">
             <h1 class="title">${values.title}</h1>
             <img src=${values.image}
                 alt="img" class="images">
